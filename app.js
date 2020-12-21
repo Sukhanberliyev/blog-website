d63d6156500a2d8317d1b18e00b2edceb16e22e2
Aidar Sukhanberliyev <sukhanberliev@gmail.com>
2020-12-22 01:21:38 +0900

    install npm and complete challenge 1 and 2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//jshint esversion:6
+
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -15,19 +15,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req,res) => {
+  
+  res.render("Home", {startingContent: homeStartingContent});
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  app.listen(3000, () => {
+    console.log("Server started on port 3000");
+  });
