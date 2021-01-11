258ae9aa51bb12afcbd1a3f44bd89b7eaa49e814
Aidar Sukhanberliyev <68350747+Sukhanberliyev@users.noreply.github.com>
2021-01-11 13:48:51 +0900

    Deploy from local to MongoDB atlas

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+// -----------Mongoose-----------
+mongoose.connect("mongodb+srv://admin-aidar:Test-123@cluster0.pxpun.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
@@ -74,7 +75,12 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+// ------------Port------------
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+  console.log("Server has started successfully");
 });
